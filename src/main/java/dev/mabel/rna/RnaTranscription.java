b45ddca0e9888d9167f3340c20e3147b4a93c1ca
MabelRincon <mabel.rincon@outlook.com>
2025-03-10 01:04:16 +0100

    feat: implement RNA transcription of Cytosine

--- src/main/java/dev/mabel/rna/RnaTranscription.java
+++ src/main/java/dev/mabel/rna/RnaTranscription.java
@@ -2,11 +2,25 @@ package dev.mabel.rna;
 
 public class RnaTranscription {
 
-    public String transcribe(String dnaStand) {
-        if (dnaStand.isEmpty()) {
+    public String transcribe(String dnaStrand) {
+        if (dnaStrand.isEmpty()) {
            return "";
+    }
+
+        StringBuilder rnaStrand = new StringBuilder();
+        for (char nucleotide : dnaStrand.toCharArray()) {
+            switch (nucleotide) {
+                case 'G':
+                    rnaStrand.append("C");
+                    break;
+                case 'C':
+                    rnaStrand.append('G');
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid nucleotide: " + nucleotide);
+            }
         }
-           return null;
+        return rnaStrand.toString();
     }
 
 }
